b93cf1f039b25aaecc66dad579e58930f43452ae
Slasher154 <thanat.varathon@gmail.com>
2013-08-28 17:33:06 +0700

    Add link to linkbudget page from welcome homepage.

--- linkbudget/urls.py
+++ linkbudget/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 from linkbudget import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index')
+                       url(r'^$', views.index, name='linkbudget_index')
 )
 
