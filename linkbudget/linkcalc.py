f16440c303797ea52ce7d488e5b08cffd89d073e
Slasher154 <thanat.varathon@gmail.com>
2013-12-12 20:35:32 +0700

    Edit carrier over noise function

--- linkbudget/linkcalc.py
+++ linkbudget/linkcalc.py
@@ -467,7 +467,7 @@ class Link:
         return 10 * log10(bandwidth * 10 ** 6)
 
     def carrier_over_noise(self):
-        pass
+        return 30
 
     def carrier_over_noise_total(self, cn_uplink, cn_downlink, ci_uplink=50, ci_downlink=50):
         """
